221ed969abc3018e90a7bb779146d89449263823
TejasBhovad <tejasbhovad@gmail.com>
2024-10-26 06:00:44 +0530

    chore: ui changes

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { loginWithGoogle } from "@/lib/auth";
-import { Github } from "lucide-react";
+import Google from "@/components/icons/Google";
 
 export default function SignUpPage() {
   const [errorMessage, setErrorMessage] = useState(null);
@@ -17,23 +17,23 @@ export default function SignUpPage() {
 
   return (
     <div className="flex min-h-full items-center justify-center">
-      <div className="flex w-full max-w-md flex-col rounded-lg border-[1.5px] border-tertiary bg-secondary px-4 pb-8 pt-4 shadow-md">
+      <div className="border-tertiary flex w-full max-w-sm flex-col rounded-lg border-[1.5px] border-white/15 bg-background px-4 pb-8 pt-4 shadow-md">
         {errorMessage && (
           <div className="mb-4 rounded-lg bg-red-100 p-4 text-sm text-red-700 dark:bg-red-200 dark:text-red-800">
             {errorMessage}
           </div>
         )}
 
-        <span className="w-full items-center justify-center text-center text-xl font-semibold text-gray-300">
-          Sign in to GitKnit
+        <span className="w-full items-center justify-center text-center text-xl font-bold text-gray-300">
+          Sign in to Google
         </span>
         <form action={loginWithGoogle} className=" ">
           <button
             type="submit"
-            className="mt-4 w-full rounded-md border-[1.5px] border-tertiary bg-foreground py-2 text-lg font-medium text-white transition-colors hover:border-accent/50 hover:bg-foreground/80 focus:outline-none focus:ring-2 focus:ring-accent"
+            className="border-tertiary items-centerborder-secondary mt-4 flex w-full justify-start gap-2 rounded-md border-[1.5px] border-secondary bg-background px-4 py-2 text-lg font-medium text-white transition-colors hover:border-accent/50 hover:bg-foreground/80 focus:outline-none focus:ring-2 focus:ring-accent"
           >
-            <Github size={24} className="mr-2 inline-block text-accent" />
-            Sign up with Google (ignore the logo)
+            <Google size={24} className="mr-2 inline-block text-accent" />
+            Sign up with Google
           </button>
         </form>
       </div>
--- src/components/login-component.jsx
+++ src/components/login-component.jsx
@@ -1,49 +1,52 @@
 "use client";
-import React, { useContext } from 'react';
-import { useRouter } from 'next/navigation';
-import { Card, CardHeader, CardTitle, CardContent, CardDescription } from './ui/card';
-import { Button } from './ui/button';
-import { logout } from '@/lib/auth';
-import { AuthContext } from '@/context/auth';
+import React, { useContext } from "react";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import { logout } from "@/lib/auth";
+import { AuthContext } from "@/context/auth";
 
 const LoginComponent = ({ name, email }) => {
-    const router = useRouter();
-    const { handleLogout } = useContext(AuthContext);
+  const router = useRouter();
+  const { handleLogout } = useContext(AuthContext);
 
-    // if no user redirect home page
+  // if no user redirect home page
 
-    const doSignOut = async () => {
-        try {
-            await handleLogout(); // Call the logout function
-            router.push('/'); // Redirect to the home page
-            
-        } catch (error) {
-            console.error("Error signing out:", error);
-        }
-    };
+  const doSignOut = async () => {
+    try {
+      await handleLogout(); // Call the logout function
+      router.push("/"); // Redirect to the home page
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
 
-    
-
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <Card className="w-80">
-                <CardHeader>
-                    <p>Name</p>
-                    <CardTitle>{name}</CardTitle>
-                    <p>Email</p>
-                    <CardDescription>{email}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <Button 
-                        onClick={doSignOut} 
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    >
-                        Sign Out
-                    </Button>
-                </CardContent>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="flex h-full items-start justify-center bg-background py-24">
+      <Card className="w-80 border-white/15 bg-background text-white">
+        <CardHeader>
+          <CardTitle>Name</CardTitle>
+          <CardDescription>{name}</CardDescription>
+          <CardTitle>Email</CardTitle>
+          <CardDescription>{email}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button
+            onClick={doSignOut}
+            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+          >
+            Sign Out
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default LoginComponent;
