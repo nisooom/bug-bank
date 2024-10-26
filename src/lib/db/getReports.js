628d80e7e4023631ed4ee58e4ea0a90a786ff07e
nisooom <shahnishumbh55@gmail.com>
2024-10-26 06:08:23 +0530

    chore: add user when creating new context

--- src/lib/db/getReports.js
+++ src/lib/db/getReports.js
@@ -1,11 +1,6 @@
 "use server";
-
-import { Client, Databases } from "appwrite";
-import { Query } from "appwrite";
-
-const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT ?? "");
+import { client } from "../auth";
+import { Databases } from "appwrite";
 
 const databases = new Databases(client);
 
--- src/providers/auth.js
+++ src/providers/auth.js
@@ -3,6 +3,12 @@ import { checkLoggedIn } from "@/lib/auth";
 import React, { useState, useEffect } from "react";
 import { AuthContext } from "@/context/auth";
 import { logout } from "@/lib/auth";
+import { addUserToDatabase } from "@/lib/db/addNewUser";
+
+async function addUser(user){
+  await addUserToDatabase(user);
+}
+
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -24,11 +30,16 @@ export const AuthProvider = ({ children }) => {
       }
       setUser(user);
       setLoading(false);
+      
  
     };
 
     fetchUser();
   }, [mounted]);
+
+  if (user && !loading) {
+    addUser(user);
+  }
 
   return (
     <AuthContext.Provider value={{ user, setUser, loading,handleLogout,setIsAuthenticated }}>
