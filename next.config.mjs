cda73d07642bba1b9d592acb9ec3ad3aa58e7c10
Siddharth Sahani <sidhucroedde@gmail.com>
2024-10-29 21:26:25 +0530

    chore: added another host name for image loading (testing)

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      // temp -->
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
