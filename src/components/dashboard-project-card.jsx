19978fbbb84bed30268c8ebd5c2154d81c17c078
Siddharth Sahani <sidhucroedde@gmail.com>
2024-10-31 10:43:19 +0530

    chore: project card now shows open and closed bug counts

--- src/components/dashboard-project-card.jsx
+++ src/components/dashboard-project-card.jsx
@@ -1,5 +1,7 @@
 import { Album } from "lucide-react";
 import Link from "next/link";
+
+
 export const ProjectCard = ({ project }) => {
   return (
     <Link
@@ -15,14 +17,25 @@ export const ProjectCard = ({ project }) => {
         <div className="h-1/3 overflow-hidden text-ellipsis whitespace-nowrap text-xl font-semibold">
           {project.name}
         </div>
-        <div className="text h-2/3 w-full">
-          {" "}
-          {/* <div className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-            {project.bugsOpened} bugs opened
+        <div className="text h-2/3 pl-2">
+          <div className="flex overflow-hidden">
+            Open Bugs:
+            <div className="px-2">
+              {project.bugs.reduce(
+                (total, bug) => total + (bug.status === "InProgress" ? 1 : 0),
+                0,
+              )}
+            </div>
           </div>
-          <div className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-            {project.bugsClosed} bugs closed
-          </div> */}
+          <div className="flex overflow-hidden">
+            Closed Bugs:
+            <div className="px-2">
+              {project.bugs.reduce(
+                (total, bug) => total + (bug.status !== "InProgress" ? 1 : 0),
+                0,
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </Link>
